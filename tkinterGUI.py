9955ef6920a016a929637798823f8d8ab98cb90e
flyfuri <93001340+flyfuri@users.noreply.github.com>
2023-10-19 21:32:17 +1300

    added colors and description

--- tkinterGUI.py
+++ tkinterGUI.py
@@ -17,7 +17,9 @@ class GUI:
         self._returnvalue = [0,0]
         self._rootW = tk.Tk(screenName="InteractiveCV")
         #self._frm = tk.Frame(self._rootW,height=600, width=800, )
-        self._rootW.geometry("1200x800")
+        self._rootW.geometry("1200x500")
+        self._rootW.configure(bg="light blue")
+        self._rootW.title("Welcome to my interactive CV")
 
     def _setWinSize(self, w=0, h=0):
         win_w, win_h = self._rootW.winfo_width() , self._rootW.winfo_height() #ToDo not working allways 1
@@ -48,15 +50,24 @@ class GUI:
         #winwidth = sum(col_wds)
         #self._setWinSize(w=winwidth)
           
-        self._frm = tk.Frame(self._rootW,height=600, width=800, )
+        self._frm = tk.Frame(self._rootW, height=600, width=800, background="light blue", padx=10, pady=10 )
         self._frm.grid()
         self._colsToSkip = 1 if withoutFirstCol else 0
+        tk.Label(self._frm, text="To filter, click the corresponding cell.", justify="left", background="light blue").grid(column=1, row=1, columnspan=6, sticky="w")
+        tk.Label(self._frm, text="To sort, click the corresponding header cell.", justify="left", background="light blue" ).grid(column=1, row=2, columnspan=6, sticky="w")
+        tk.Label(self._frm, text="To change sort order, click header cell off alredy sorted column again.", justify="left", background="light blue" ).grid(column=1, row=3, columnspan=6, sticky="w")
+        tk.Label(self._frm, text=" ", justify="left", background="light blue" ).grid(column=1, row=4, columnspan=6)
+        xoffs, yoffs = 1, 5
         for y, row in enumerate(tablerecords):
             for x, celltxt in enumerate(row[self._colsToSkip : ]):
-                tk.Button(self._frm, text=celltxt, command=partial(self._callback_tablebutton, x, y), width=col_wds[x+self._colsToSkip],).grid(column=x+1, row=y+1)
+                tk.Button(self._frm, text=celltxt, 
+                          command=partial(self._callback_tablebutton, x, y), 
+                          width=col_wds[x+self._colsToSkip], background=("gray" if y == 0 else "light gray")).grid(column=x+xoffs, row=y+yoffs)
                 px, py = x, y
-        tk.Button(self._frm, text="reset filter", command=self._callback_resfilt, width=15).grid(column=px, row=py+4)
-        tk.Button(self._frm, text="quit", command=self._callback_quit, width=15).grid(column=px+1, row=py+4, padx=10, pady=10)
+        px = 5
+        py += (yoffs+1)
+        tk.Button(self._frm, text="reset filter", command=self._callback_resfilt, width=15).grid(column=px, row=py, padx=10, pady=10)
+        tk.Button(self._frm, text="quit", command=self._callback_quit, width=15).grid(column=px+1, row=py, padx=10, pady=10)
         self._frm.mainloop()
         return self._returnvalue
   
